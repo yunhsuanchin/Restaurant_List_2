b241f877c70593a7eef0d511cccebebb82d655a2
yunhsuanchin <alicechin20181@gmail.com>
2020-10-19 21:37:49 +0800

    feat: add sort-selection function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,4 +14,40 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// sort --> name-asc
+router.get('/name-asc', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort('name')
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// sort --> name-desc
+router.get('/name-desc', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort('-name')
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// sort --> id-asc
+router.get('/id-asc', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort('_id')
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// sort --> id-desc
+router.get('/id-desc', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort('-_id')
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
