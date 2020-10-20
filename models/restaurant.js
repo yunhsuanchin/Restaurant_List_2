8e631be54130bcd44354443072c35fcf139b0dbd
yunhsuanchin <alicechin20181@gmail.com>
2020-10-20 15:34:00 +0800

    fix: modify restaurant model required data to meet sort function

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: { type: String, required: true },
   name_en: { type: String, required: false },
-  category: { type: String, required: false },
+  category: { type: String, required: true },
   image: { type: String, required: true },
-  location: { type: String, required: false },
+  location: { type: String, required: true },
   phone: { type: String, required: false },
   google_map: { type: String, required: false },
   rating: { type: Number, required: false },
